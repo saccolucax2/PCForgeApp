9e7e6c9d7d72243804e469fb1f140bada9d79314
Luca Sacco <saccoluca9@gmail.com>
2025-05-14 18:15:02 +0200

    Initial commit

---
+++ src/main/java/it/unisannio/pcforgeapp/PcForgeAppApplication.java
@@ -0,0 +1,13 @@
+package it.unisannio.pcforgeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcForgeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcForgeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unisannio/pcforgeapp/PcForgeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisannio.pcforgeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcForgeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
